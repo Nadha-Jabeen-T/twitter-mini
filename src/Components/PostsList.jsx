d22a009766ab0a971f7b419beb305079144a5e88
Nadha Jabeen <nadha.jabeen@gmail.com>
2024-05-15 07:36:48 +0530

    States, state management and lifting with NewPost component

--- src/Components/PostsList.jsx
+++ src/Components/PostsList.jsx
@@ -1,12 +1,32 @@
 
 import Post from "./Post";
 import classes from "./PostsList.module.css";
+import NewPost from "./NewPost";
+
+import { useState } from "react";
+
 function PostsList() {
+    const [enteredBody,setEnteredBody] = useState("");
+    //setEnteredBody is a function that updates useState[0]
+
+    const [enteredAuthor,setEnteredAuthor] = useState("");
+
+    function bodyChangeHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+
+    function authorChangeHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
+
     return(
+        <>
+        <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler}/>
         <ul className={classes.posts}>
-            <li><Post author="Joe" body="D Day coming"/></li>
-            <li><Post author="Zayn" body="Stranger Things"/></li>
+            <li><Post author={enteredAuthor} body={enteredBody}/></li>
+            <li><Post author={enteredAuthor} body={enteredBody}/></li>
         </ul>
+        </>
     );
 }
 export default PostsList;
