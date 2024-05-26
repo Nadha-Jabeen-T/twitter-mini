0c65b7d28cfbd02b65c15215b88befd94a147f21
Nadha Jabeen <nadha.jabeen@gmail.com>
2024-05-26 15:31:27 +0530

    Added basic routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import App from './App'
 import './index.css'
+import NewPost from './Components/NewPost';
+
+const router = createBrowserRouter([
+  {path: '/', element: <App />}, //<our-domain>--landing page
+  {path: '/create-post', element: <NewPost /> }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
