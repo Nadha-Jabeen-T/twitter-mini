9381c2e093a55bf14d3724e4d47cd4bbf74df534
Nadha Jabeen <nadha.jabeen@gmail.com>
2024-05-26 13:56:21 +0530

    Used fetch() to send data | Used useEffect hook for initials loading of posts from backend

--- src/Components/PostsList.jsx
+++ src/Components/PostsList.jsx
@@ -4,13 +4,31 @@ import classes from "./PostsList.module.css";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 
-import { useState } from "react";
+import { useState , useEffect} from "react";
 
 function PostsList({isPosting, onStopPosting}) {
-    const [posts,setPosts] = useState([]);//To maintain the list of posts
+
+       const [posts,setPosts] = useState([]);//To maintain the list of posts
+
+    useEffect(() => {
+        async function fetchPosts() {
+            const response = await fetch('http://localhost:8080/posts');
+            const resData = await response.json();
+            setPosts(resData.posts);
+        }
+        fetchPosts();
+    },[]);
 
     //To add the new post component whenever we get new postData
     function addPostHandler(postData) {
+        //To send the posts to db and store it there when we create a new post.
+        fetch('http://localhost:8080/posts',{
+            method: 'POST',
+            body: JSON.stringify(postData),
+            headers:{
+                'Content-Type': 'application/json'
+            }
+        });
         setPosts((existingPosts) => [postData,...existingPosts]);//Passing a new array to setPosts + combining the things from the previous posts array using spread operator
     }
 
