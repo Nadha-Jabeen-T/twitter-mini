5ab5101d16a16196e95c9baf0211659409dcf632
Nadha Jabeen <nadha.jabeen@gmail.com>
2024-05-26 19:34:07 +0530

    Added loader to the Posts route and fetched the data accordingly

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import Posts from './routes/Posts'
+import Posts,{loader as postsLoader} from './routes/Posts'
 import './index.css'
 import NewPost from './routes/NewPost';
 import RootLayout from './routes/RootLayout';
 
 const router = createBrowserRouter([
   {path: '/', element: <RootLayout />, children: [
-    {path: '/', element: <Posts />, children: [
+    {
+      path: '/', 
+      element: <Posts />, 
+      loader: postsLoader,
+      children: [
       {path: '/create-post', element: <NewPost /> }
     ]} //<our-domain>--landing page
   ]}, //Layout route - Routes that go with all the other routes or wraps them
